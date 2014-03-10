0504a5a881c87937a670e3bf4bc100a682430155
picnii <sompop@picnii.me>
2014-03-10 15:51:10 +0700

    rootscope

--- js/Controller.js
+++ js/Controller.js
@@ -1,17 +1,19 @@
-function MainCtrl($scope)
+function MainCtrl($scope, $rootScope)
 {
 	$scope.test = "hello world";
+	$rootScope.todsob  = "THIS IS FROM MAIN";
+	
 }
 
 function BillCtrl($scope)
 {
-	$scope.bills = [
+	/*$scope.bills = [
 		{name:"ค่าน้ำ", price:20},
 		{name:"ค่าไฟ", price:200}
-	]
+	]*/
 }
 
-function ProductCtrl($scope)
+function ProductCtrl($scope, $rootScope)
 {
 	$scope.products = [
 		{name:"water", price:20, count:1,
@@ -45,10 +47,23 @@ function ProductCtrl($scope)
 
 	$scope.updatePrice = function()
 	{
+		//เครียร์บิล ให้ไม่มีสินค้าเลย
+		$rootScope.bills = [];
+
 		$scope.sum = 0;
 		for(var i=0; i < $scope.products.length; i++)
 		{
 			$scope.sum += $scope.products[i].price * $scope.products[i].count
+			//update bills
+			//check wa product > 0 piece mai?
+			if($scope.products[i].count > 0)
+			{	
+				//create bill object
+				var bill_price = $scope.products[i].price * $scope.products[i].count;
+				var bill = {name:$scope.products[i].name, price:bill_price};
+				$rootScope.bills.push(bill);
+			}
+
 		}
 	}
 
@@ -60,7 +75,7 @@ function ContactCtrl($scope)
 	
 }
 
-function AboutCtrl($scope)
+function AboutCtrl($scope, $rootScope)
 {
-
+	$scope.todsob = " THIS IS FROM ABOUT"
 }
