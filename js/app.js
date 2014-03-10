852e72a7cfee90ef9248ad48583fce24d8afc1ea
picnii <sompop@picnii.me>
2014-03-10 14:17:27 +0700

    update mg-route 1

---
+++ js/app.js
@@ -0,0 +1,19 @@
+var app = angular.module('route', [
+  'ngRoute'
+]);
+ /*
+phonecatApp.config(['$routeProvider',
+  function($routeProvider) {
+    $routeProvider.
+      when('/phones', {
+        templateUrl: 'partials/phone-list.html',
+        controller: 'PhoneListCtrl'
+      }).
+      when('/phones/:phoneId', {
+        templateUrl: 'partials/phone-detail.html',
+        controller: 'PhoneDetailCtrl'
+      }).
+      otherwise({
+        redirectTo: '/phones'
+      });
+  }]);*/
